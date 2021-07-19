69c1a8bcd2853798774660ea3bafb857d4fe2433
Vitaliy <51445834+OrangeMGE@users.noreply.github.com>
2021-07-20 01:56:02 +0300

    Added a transition to the page with hotels
    Added a transition to the page with hotels.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { HotelsPage } from '../hotels/hotels';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  constructor(public navCtrl: NavController) { }
 
-  constructor(public navCtrl: NavController) {
-
+  hotelSelectionButton() {
+    this.navCtrl.push(HotelsPage)
   }
-
 }
